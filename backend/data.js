20f22c65aa3216f3f3ad3c4dcc7b9fa605e1a876
Romain Coupey <schaft115@hotmail.com>
2020-08-22 19:16:55 +0200

    add express server

---
+++ backend/data.js
@@ -0,0 +1,26 @@
+export default {
+  products:[{
+    _id: '1',
+    name: 'Anthropométrie',
+    maker: 'Yves Klein',
+    image: '/images/d1.jpg',
+    category: 'painting',
+    price: 9000
+  },
+  {
+    _id: '2',
+    name: 'Cosmogonie',
+    maker: 'Yves Klein',
+    image: '/images/d1.jpg',
+    category: 'painting',
+    price: 2000
+  },
+  {
+    _id: '3',
+    name: 'Monochrome',
+    maker: 'Yves Klein',
+    image: '/images/d1.jpg',
+    category: 'lithography',
+    price: 150
+  }]
+}
