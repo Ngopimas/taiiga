d43bef18dd9ed63e5bddaca9bdb4301b5e37c0d2
Romain Coupey <schaft115@hotmail.com>
2020-08-23 01:13:10 +0200

    solved redux thunk prob

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,9 +4,8 @@ import axios from 'axios';
 const ListProducts = () => async dispatch => {
 
   try {
-    dispatch(PRODUCT_LIST_REQUEST);
+    dispatch({ type: PRODUCT_LIST_REQUEST });
     const {data} = await axios.get("/api/products");
-    console.log(data)
     dispatch({type: PRODUCT_LIST_SUCCESS, payload: data })
   }
   catch(error){
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,11 +1,11 @@
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants";
 
-function productListReducer(state= { products: [] }, action){
+function productListReducer(state = { products: [] }, action){
 
   switch (action.type) {
 
     case PRODUCT_LIST_REQUEST:
-      return {loading: true};
+      return {loading: true, products: [] };
     case PRODUCT_LIST_SUCCESS:
       return {loading: false, products: action.payload};
     case PRODUCT_LIST_FAIL:
