38deb3c42f18d93d29bd0c81cb0d0c8642ebf477
Romain Coupey <schaft115@hotmail.com>
2020-08-22 17:19:35 +0200

    HomeScreen done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import data from './data';
 import './App.css';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add('open');
+  }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove('open');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>
+              &#9776;
+            </button>
+            <Link to="/">taiiga</Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign in</a>
+          </div>
+        </header>
+
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close" onClick={closeMenu}>X</button>
+          <ul>
+            <li><a href="index.html">Painting</a></li>
+            <li><a href="index.html">Lithography</a></li>
+          </ul>
+        </aside>
+
+        <main className="main">
+          <div className="content">
+          <Route path="/products/:id" component={ProductScreen} />
+          <Route path="/" exact={true} component={HomeScreen} />
+
+          </div>
+        </main>
+
+        <footer className="footer">
+          All rights reserved.
+        </footer>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,23 @@
+export default {
+  products:[{
+    name: 'Anthropométrie',
+    maker: 'Yves Klein',
+    image: '/images/d1.jpg',
+    category: 'painting',
+    price: 9000
+  },
+  {
+    name: 'Cosmogonie',
+    maker: 'Yves Klein',
+    image: '/images/d1.jpg',
+    category: 'painting',
+    price: 2000
+  },
+  {
+    name: 'Monochrome',
+    maker: 'Yves Klein',
+    image: '/images/d1.jpg',
+    category: 'lithography',
+    price: 150
+  }]
+}
